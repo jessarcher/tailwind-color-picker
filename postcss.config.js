95f4dd02a7dceb4e11e493b39f4235b6778f544e
Jess Archer <jess@jessarcher.com>
2019-03-14 13:55:58 +1000

    Add PurgeCSS

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+const tailwindcss = require("tailwindcss");
+
+const autoprefixer = require("autoprefixer");
+
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+module.exports = {
+
+    plugins: [
+
+        tailwindcss("./tailwind.js"),
+
+        autoprefixer({
+            add: true,
+            grid: true
+        }),
+
+        //Only add purgecss in production
+        process.env.NODE_ENV === "production"? purgecss({
+            content: [
+                "./src/**/*.html", 
+                "./src/**/*.vue"
+            ]
+        }): ""
+    ]
+};
